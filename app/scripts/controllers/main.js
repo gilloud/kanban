ee4c7d2d16f3443768042da8dff6b0ab638e9470
gilles <gilles@pilloud.fr>
2013-12-22 16:50:44 +0100

    layout changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,11 +10,11 @@ angular.module('kanbanApp').service('kanbanService', function () {
                 'value': [{
                     'id': '123',
                     'title': 'task1',
-                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
-                    'owner': 'gilles'
+'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',                    'owner': 'gilles'
                 }, {
                     'id': '124',
                     'title': 'task2',
+              'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     'owner': 'gilles'
                 }, {
                     'id': '125',
@@ -26,6 +26,7 @@ angular.module('kanbanApp').service('kanbanService', function () {
                     'owner': 'gilles'
                 }, {
                     'id': '127',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     'title': 'task5',
                     'owner': 'gilles'
                 }]
@@ -35,25 +36,55 @@ angular.module('kanbanApp').service('kanbanService', function () {
                 'value': [{
                     'id': '128',
                     'title': 'task6',
-                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     'owner': 'gilles'
                 }]
             }, {
                 'title': 'in progress',
                 'taskOrder': '3',
-                'value': []
+                'value': [ {
+                    'id': '132',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+
+                    'title': 'task9',
+                    'owner': 'gilles'
+                }, {
+                    'id': '133',
+'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'title': 'task9',
+                    'owner': 'gilles'
+                }]
             }, {
                 'title': 'unit testing',
                 'taskOrder': '4',
-                'value': []
+                'value': [ {
+                    'id': '134',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+                    'title': 'task9',
+                    'owner': 'gilles'
+                }]
             }, {
                 'title': 'waiting integration',
                 'taskOrder': '5',
-                'value': []
+                'value': [ {
+                    'id': '134',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+
+                    'title': 'task9',
+                    'owner': 'gilles'
+                }]
             }, {
                 'title': 'integration',
                 'taskOrder': '6',
-                'value': []
+                'value': [ {
+                    'id': '135',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+
+                    'title': 'Review of a little piece of code',
+                    'owner': 'gilles'
+                }]
             }, {
                 'title': 'done',
                 'taskOrder': '7',
@@ -67,7 +98,7 @@ angular.module('kanbanApp').service('kanbanService', function () {
                     'owner': 'gilles'
                 }, {
                     'id': '131',
-                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 
                     'title': 'task9',
                     'owner': 'gilles'
@@ -91,11 +122,10 @@ angular.module('kanbanApp')
             'done'
         ];
         $scope.stepsObjects = kanbanService.getTasks();
-        console.log($scope.stepsObjects);
 
         $scope.goLeft = function (id, pos) {
             alert('Item ' + id + ' has been moved left ' + pos);
-            // Call to webservice andg
+            // Call to webservice and
 
             // Refresh datas...
             $scope.stepsObjects = kanbanService.getTasks();
