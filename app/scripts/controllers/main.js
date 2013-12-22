4320237880ca953d2239447f81e403ca113c1643
gilles <gilles@pilloud.fr>
2013-12-22 08:41:04 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,148 @@
+'use strict';
+
+angular.module('kanbanApp').service('kanbanService', function () {
+
+    return {
+        getTasks: function () {
+            return [{
+                'title': 'backlog',
+                'taskOrder': '1',
+                'value': [{
+                    'id': '123',
+                    'title': 'task1',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+                    'owner': 'gilles'
+                }, {
+                    'id': '124',
+                    'title': 'task2',
+                    'owner': 'gilles'
+                }, {
+                    'id': '125',
+                    'title': 'task3',
+                    'owner': 'gilles'
+                }, {
+                    'id': '126',
+                    'title': 'task4',
+                    'owner': 'gilles'
+                }, {
+                    'id': '127',
+                    'title': 'task5',
+                    'owner': 'gilles'
+                }]
+            }, {
+                'title': 'todo',
+                'taskOrder': '2',
+                'value': [{
+                    'id': '128',
+                    'title': 'task6',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+                    'owner': 'gilles'
+                }]
+            }, {
+                'title': 'in progress',
+                'taskOrder': '3',
+                'value': []
+            }, {
+                'title': 'unit testing',
+                'taskOrder': '4',
+                'value': []
+            }, {
+                'title': 'waiting integration',
+                'taskOrder': '5',
+                'value': []
+            }, {
+                'title': 'integration',
+                'taskOrder': '6',
+                'value': []
+            }, {
+                'title': 'done',
+                'taskOrder': '7',
+                'value': [{
+                    'id': '129',
+                    'title': 'task7',
+                    'owner': 'gilles'
+                }, {
+                    'id': '130',
+                    'title': 'task8',
+                    'owner': 'gilles'
+                }, {
+                    'id': '131',
+                    'description': 'Je teste une petite description courte... Je teste une deuxième description courte... Je teste une 3ème description courte...',
+
+                    'title': 'task9',
+                    'owner': 'gilles'
+                }]
+            }];
+
+        }
+    };
+
+});
+
+angular.module('kanbanApp')
+    .controller('MainCtrl', function ($scope, kanbanService) {
+        $scope.steps = [
+            'backlog',
+            'todo',
+            'in progress',
+            'unit testing',
+            'waiting integration',
+            'integration',
+            'done'
+        ];
+        $scope.stepsObjects = kanbanService.getTasks();
+        console.log($scope.stepsObjects);
+
+        $scope.goLeft = function (id, pos) {
+            alert('Item ' + id + ' has been moved left ' + pos);
+            // Call to webservice and
+
+            // Refresh datas...
+            $scope.stepsObjects = kanbanService.getTasks();
+        };
+        
+        $scope.goRight = function (id, pos) {
+            alert('Item ' + id + ' has been moved right ' + pos);
+            // Call to webservice and
+
+            // Refresh datas...
+            $scope.stepsObjects = kanbanService.getTasks();
+        };
+
+        $scope.goElement = function (id) {
+            alert('Going to element ' + id);
+        };
+
+
+        $scope.hasLeft = function (pos) {
+            var tmpPos = pos;
+            var elems = $scope.stepsObjects;
+            for (var item in elems) {
+                if (elems[item].taskOrder < tmpPos) {
+                    tmpPos = elems[item].taskOrder;
+                }
+            }
+
+            if (tmpPos < pos)
+            {
+                return true;
+            }
+            return false;
+        };
+
+        $scope.hasRight = function (pos) {
+            var tmpPos = pos;
+            var elems = $scope.stepsObjects;
+            for (var item in elems) {
+                if (elems[item].taskOrder > tmpPos) {
+                    tmpPos = elems[item].taskOrder;
+                }
+            }
+
+            if (tmpPos > pos)
+            {
+                return true;
+            }
+            return false;
+        };
+    });
---
+++ readme.txt
@@ -0,0 +1,27 @@
+J1 :
+Début du projet 21/12 : 13:30 - 14:11                       ### 40m
+    structure, grunt, yehoman,
+    json de données
+    objet html, JS
+
+20:33 - 21h40                                               ### 60m
+    json de données (mal décrit au début)
+    ordonnancement des colonnes (doit être un tableau...) 
+
+21:40 - 22:30                                               ### 50m
+    test drag drop. Trop complexe pr le besoin retour aux boutons
+
+22:30 - 23:40                                               ### 70m
+    boutons de déplacement
+    finalisation js, css
+    boutons en utf-8
+    ajout commentaire
+
+
+====================================================================
+Total : 220mn => 3h40
+
+la définition structure json est primordiale et ne doit pas être loupée, bien réfléchie  (30mn perdue)
+Le besoin d'outils (drag drop) doit être pesé, entre le nombre d'utilisation, complexité à implémenter... (1h perdue)
+
+Sans les pb : 130m => 2h10
