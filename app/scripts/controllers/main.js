ddd744c402627aee831ffd9118d37905a515a22c
gilles <gilles@pilloud.fr>
2014-01-10 21:16:50 +0100

    Titles changes, and javascript improvement for text length

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@ angular.module('kanbanApp').service('kanbanService', function () {
                 'taskOrder': '1',
                 'value': [{
                     'id': '123',
-                    'title': 'task1',
+                    'title': '1234 - Le truc que tu veux. Le nom de mon titre qui est trop long',
 'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',                    'owner': 'gilles'
                 }, {
                     'id': '124',
@@ -26,6 +26,26 @@ angular.module('kanbanApp').service('kanbanService', function () {
                     'owner': 'gilles'
                 }, {
                     'id': '127',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'title': 'task5',
+                    'owner': 'gilles'
+                }, {
+                    'id': '1127',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'title': 'task5',
+                    'owner': 'gilles'
+                }, {
+                    'id': '1128',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'title': 'task5',
+                    'owner': 'gilles'
+                }, {
+                    'id': '1129',
+                    'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'title': 'task5',
+                    'owner': 'gilles'
+                }, {
+                    'id': '1130',
                     'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     'title': 'task5',
                     'owner': 'gilles'
@@ -115,6 +135,8 @@ angular.module('kanbanApp')
             'integration',
             'done'
         ];
+
+        $scope.application_name= 'Ķăņbąń dashboard';
         $scope.stepsObjects = kanbanService.getTasks();
 
         $scope.colWidth = function(){
@@ -164,5 +186,25 @@ angular.module('kanbanApp')
                 }
             }
             return false;
+        };
+    });
+
+angular.module('kanbanApp').filter('cut', function () {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || '…');
         };
     });
